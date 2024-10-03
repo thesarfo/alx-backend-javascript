2982af3c9cbf52f828084c65d3bdd777bf70f601
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-03 17:12:12 +0000

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((res, rej) => {
+    if (success) {
+      res({ status: 200, body: 'Success' });
+    } else {
+      rej(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
+/* eslint-disable eol-last */
