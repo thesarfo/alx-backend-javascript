5add65ccad4f74dfc4dbf673ae62f86f40bfbfc6
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-10-03 17:13:17 +0000

    task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
+
+export default guardrail;
